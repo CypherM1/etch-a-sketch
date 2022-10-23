d23b0f5b7b30576ebe906e7708aef40d66ee5f26
Justin Sears <justinrsears@gmail.com>
2022-10-22 21:04:58 -0700

    Added JS to set number of sqaures based on input from user.

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const containerDiv = document.getElementById('container');
-
-
-
-//const innerDiv = document.createElement('div');
-//innerDiv.className = 'block';
-
-//containerDiv.appendChild(innerDiv);
-
 let innerDivs = document.createDocumentFragment();
+let sqrNum = prompt('How many squares do you want?')
+let sqrWh = setWh(sqrNum);
+console.log(sqrWh);
 
-for(var i=0; i < 1024; i++){
+for(var i=0; i < sqrNum; i++){
    var newDiv = document.createElement('div');
    newDiv.id = 'r'+i;
    newDiv.className = 'block';
    innerDivs.appendChild(newDiv);
 }
 
+function setWh(num) {
+    let numSq = Math.sqrt(num);
+    wH = 600 / numSq;
+    return wH;
+}
+
 containerDiv.appendChild(innerDivs);
+
+var widthHeight = document.getElementsByClassName('block');
+Array.from(widthHeight).forEach(function(widthHeight) {
+    widthHeight.style.height = `${sqrWh}px`;
+    widthHeight.style.width = `${sqrWh}px`;
+});
